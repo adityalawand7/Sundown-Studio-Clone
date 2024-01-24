bdd6320ef473855267433b636f96766de98108c9
adityalawand7 <lawandaditya007@gmail.com>
2024-01-24 22:48:42 +0530

    step 4

--- logic.js
+++ logic.js
@@ -5,9 +5,33 @@ const scroll = new LocomotiveScroll({
 
 var fixedImage = document.querySelector("#fixed-image")
 var elemC = document.querySelector("#elem-container")
-elemC.addEventListener("mouseenter", ()=>{
+
+elemC.addEventListener("mouseenter", () => {
     fixedImage.style.display = "block"
 })
-elemC.addEventListener("mouseleave", ()=>{
+elemC.addEventListener("mouseleave", () => {
     fixedImage.style.display = "none"
+})
+
+// var elem1 = document.getElementById("elem1")
+// elem1.addEventListener("mouseenter", () => {
+//     var imageUrl = elem1.getAttribute("data-image")
+//     fixedImage.style.backgroundImage = `url(${imageUrl})`
+// })
+// elem1.addEventListener("mouseleave", () => {
+//     var imageUrl = elem1.getAttribute("data-image")
+//     fixedImage.style.background = "transparent"
+// })
+
+var elems = document.querySelectorAll(".elem")
+elems.forEach((e) => {
+    e.addEventListener("mouseenter", () => {
+        fixedImage.style.zIndex = "99"
+        var imageUrl = e.getAttribute("data-image")
+        fixedImage.style.backgroundImage = `url(${imageUrl})`
+    })
+    // e.addEventListener("mouseleave", () => {
+    //     var imageUrl = elem1.getAttribute("data-image")
+    //     fixedImage.style.background = "transparent"
+    // })
 })
