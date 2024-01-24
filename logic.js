1e2f564dfd90a15cc34f13e46697b835cd45bd5c
adityalawand7 <lawandaditya007@gmail.com>
2024-01-24 19:16:47 +0530

    step 3

--- logic.js
+++ logic.js
@@ -2,3 +2,12 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var fixedImage = document.querySelector("#fixed-image")
+var elemC = document.querySelector("#elem-container")
+elemC.addEventListener("mouseenter", ()=>{
+    fixedImage.style.display = "block"
+})
+elemC.addEventListener("mouseleave", ()=>{
+    fixedImage.style.display = "none"
+})
